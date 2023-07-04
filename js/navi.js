78ee11758d8fddbf20f8429df5cc5d98b897c39a
Sasarino MARi <develop.mari@gmail.com>
2023-07-05 01:58:51 +0900

    본문 가져오기

---
+++ js/navi.js
@@ -0,0 +1,15 @@
+function onClickHome(e) {
+    document.getElementById("header").scrollIntoView({behavior: "smooth"});
+}
+
+function onClickTalent(e) {
+    document.getElementById("talent-container").scrollIntoView({behavior: "smooth"});
+}
+
+function onClickContent(e) {
+    document.getElementById("content-container").scrollIntoView({behavior: "smooth"});
+}
+
+function onClickGuide(e) {
+    document.getElementById("guide-container").scrollIntoView({behavior: "smooth"});
+}
